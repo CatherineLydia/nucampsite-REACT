45734e5a4ed2ff0a9549a844266e11df71fd99e8
Catherine Lydia <p.cath.reens@gmail.com>
2021-04-26 16:17:29 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -19,7 +19,7 @@ function RenderCampsite({campsite}) {
     );
 }
         
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -33,7 +33,7 @@ function RenderComments({comments}) {
                         </p>
                     </div>
                 )}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
             </div>
         );
     }
@@ -47,10 +47,8 @@ class CommentForm extends Component{
     constructor(props) {
         super(props);
 
-        this.state = {
-                
+        this.state = {      
             isModalOpen: false,
-            
         }
         
         this.toggleModal = this.toggleModal.bind(this);
@@ -62,10 +60,12 @@ class CommentForm extends Component{
             isModalOpen: !this.state.isModalOpen
         });
     }
+
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
+
     render() {
         return (
             <React.Fragment>
@@ -159,7 +159,10 @@ function CampsiteInfo(props){
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+                                    addComment={props.addComment}
+                                    campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
